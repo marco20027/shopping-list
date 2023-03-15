34d3201f6e4e434e741d42321f9713f0115ba591
Marco <marco.campanale@par-tec.it>
2023-03-15 14:21:14 +0100

    push

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Recipe } from '../recipes.module';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
